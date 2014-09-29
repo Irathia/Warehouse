26ad9e05c5da7f63d6cb2cfe69c837784cbdc8d9
Irathia <erathia.irathia@gmail.com>
2014-09-29 14:29:23 +0400

    Add read from file

--- Items.java
+++ Items.java
@@ -1,10 +1,14 @@
 package Warehouse;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Vector;
 
 public class Items {
 	
-	Vector <String> goods;
+	Vector <Item> items;
 	
 	Items(String filename)
 	{
@@ -16,10 +20,46 @@ public class Items {
 		/*
 		 * here we create Map <Shelf, string>  and after that create variable goods based on map, like index in vector is point for save
 		 */
+		String line = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			
+			while((line = br.readLine()) != null){
+				String[] elements = line.split(";");
+				//shelf,index,-,-,rigidity
+				Item it = new Item(Integer.parseInt(elements[1]),Integer.parseInt(elements[4]));
+				items.add(Warehouse.getInstance().getIndexOfShelf(elements[0]),it);
+			}
+			
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	};
 	
-	public final String getGoods(int index)
+	public final Item getItems(int index)
 	{
-		return goods.get(index);
+		return items.get(index);
 	};
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public void deleteItem(int index){
+        for (int i = 0; i < items.size(); i++){
+            if (items.get(i).getIndex() == index){
+                items.remove(i);
+                return;
+            }
+        }
+    }
+
+    public final int getSize(){
+        return items.size();
+    }
 }
